26d1fd9cde162d43f6701d61e35d92adfa993557
JMonge123 <55443434+JMonge123@users.noreply.github.com>
2019-09-18 08:22:45 -0400

    Add files via upload

---
+++ 2019-09-18-classwork/2019-09-18-classwork/main.cpp
@@ -0,0 +1,13 @@
+// Joshua Monge
+// Intro to computer science
+// Period 1
+// Mr. Williams
+// Classwork 2019-09-18
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Joseph eats trash" << std::endl;
+
+}
