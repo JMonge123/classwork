e7c0905980cefc8e65cf953eb86aced7a4f618d2
JMonge123 <55443434+JMonge123@users.noreply.github.com>
2019-10-02 08:42:35 -0400

    Add files via upload

---
+++ 2019-10-2-Classwork/2019-10-2-Classwork/main.cpp
@@ -0,0 +1,31 @@
+// Joshua Monge
+// Intro to computer science
+// Period 1
+// Mr. williams
+// 2019-10-2
+
+#include <iostream>
+int main()
+{
+	char p = 's';
+	char j = 'c';
+	char d = 'e';
+	char r = 93;
+		
+	short life = 0;
+	short fggg = 8;
+	short djdj = 8;
+	int periods = 2;
+	int owowo = 8;
+	int ddkd = 99;
+	unsigned long joshua = 1000404994;
+	unsigned long ksk = 100002022;
+	unsigned long jdjddj = 1999292992;
+	bool oryanIsCool = false;
+	bool joshuaIsCool = true;
+	bool tgrfdd = true;
+	float guest = 992929;
+	float test = 92929;
+	float chest = 383;
+
+}
