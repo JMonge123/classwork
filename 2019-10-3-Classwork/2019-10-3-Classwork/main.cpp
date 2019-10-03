e1bc3f39728d2e55c388df0ddcadd8ae898ebc2f
JMonge123 <55443434+JMonge123@users.noreply.github.com>
2019-10-03 08:39:08 -0400

    Add files via upload

---
+++ 2019-10-3-Classwork/2019-10-3-Classwork/main.cpp
@@ -0,0 +1,17 @@
+// Joshua Monge
+// Intro to computer science
+// Period 1
+// Mr. Williams
+// 2019-10-03-Classwork
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << " Enter one side of the square:";
+	int lengthOfSquare;
+	cin >> lengthOfSquare;
+
+
+}
